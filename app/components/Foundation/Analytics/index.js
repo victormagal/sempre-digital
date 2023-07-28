5f09f42218d37ee35cd9b71e97906f6c3fab90b3
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-07-28 12:26:03 -0300

    feat: atualiza google tag manager

--- app/components/Foundation/Analytics/index.js
+++ app/components/Foundation/Analytics/index.js
@@ -20,7 +20,7 @@ export default function Analytics() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N8Z66M2');`
+          })(window,document,'script','dataLayer','G-RGW3B05WFT');`
         }}
       />
       <Script
